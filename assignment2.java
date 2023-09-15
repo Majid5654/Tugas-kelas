d457888766ee5406397a9b22eaeaee5d34a6b05f
Majid5654 <3rwanmajid@gmail.com>
2023-09-15 13:14:20 +0700

    Assignment 2

---
+++ assignment2.java
@@ -0,0 +1,28 @@
+public class assignment2 {
+public static void main(String[] args){
+char bloodgroup = 'A';
+byte distance = (byte) 130;
+short theNumberOfResidents = 1025;
+float temperature = 60.50F;
+double weight = 0.5467812345;
+long balance = 1500000000;
+int number = 0x10;
+System.out.println("bloodgroup\t\t: " + (byte) bloodgroup);
+System.out.println("Distance\t\t: " + distance);
+System.out.println("The number of residents\t: " + theNumberOfResidents);
+System.out.println("Temperature\t\t: " + temperature);
+System.out.println("Weight\t\t\t: " + (float) weight );
+System.out.println("Balance\t\t\t: " + balance);
+System.out.println("Number\t\t\t: " + number);
+
+
+
+
+
+
+
+
+
+
+}
+}
