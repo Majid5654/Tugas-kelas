e04ae3123a7bdbcd9a596235f439d8c6333af050
Majid5654 <3rwanmajid@gmail.com>
2023-09-15 11:31:38 +0700

    Assignment 1 week 3

---
+++ barulagi.java
@@ -0,0 +1,15 @@
+public class barulagi {
+    public static void main(String[] args) {
+     String oneOfMyHobbies = "editing video";
+     boolean isSmart = true;
+     char gender = 'm';
+     byte _age = 19;
+     double $gpa = 3.38, height = 1.68;
+     System.out.println(oneOfMyHobbies);
+     System.out.println("Are you smart ?" + isSmart);
+     System.out.println("Gender:" +gender);
+     System.out.println("My current age is" +_age);
+     System.out.println(String.format("My GPA is %s and my height is %s meters",$gpa, height));
+
+    }
+}
