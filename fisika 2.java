5102f167c480cd6d0833a40e91338c3f51abdaf1
Majid5654 <3rwanmajid@gmail.com>
2023-09-16 15:43:19 +0700

    ini fisika fix

---
+++ fisika 2.java
@@ -0,0 +1,21 @@
+public class fisika 2 {
+public static void main(String[] args){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+}
---
+++ fisika2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class fisika2 {
+    public static void main(String[] args) {
+        int R1, R2, R3, R4, I;
+        double V;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan R1 : ");
+        R1 = in.nextInt();
+        System.out.print("Masukkan R2 : ");
+        R2 = in.nextInt();
+        System.out.print("Masukkan R3 : ");
+        R3 = in.nextInt();
+        System.out.print("Masukkan R4 : ");
+        R4 = in.nextInt();
+        System.out.print("Masukkan  I : ");
+        I = in.nextInt();
+
+        double Rtotal;
+        Rtotal = R4 + (1 / (1.0 / R1 + 1.0 / R2 + 1.0 / R3));
+        V = I * Rtotal;
+
+        System.out.println("Rtotal anda = " + Rtotal);
+        System.out.println("Maka V anda = " + V);
+
+        int a = 3;
+        int b = 2;
+        float c = (float) a / b; // Cast one of the operands to float for accurate division.
+        System.out.println("Hasilnya adalah = " + c);
+    }
+}
