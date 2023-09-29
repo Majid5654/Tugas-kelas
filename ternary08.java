2e359cdef9a37613872b32022347c9dcf5dc5465
Majid5654 <3rwanmajid@gmail.com>
2023-09-29 11:08:00 +0700

    week5

---
+++ ternary08.java
@@ -0,0 +1,18 @@
+public class ternary08 {
+    import java.util.Scanner;
+public static void main(String[] args) {
+Scanner input08 = new Scanner (System.in);
+System.out.println("Input a number = ");
+int number = input08.nextInt();
+
+if(number%2 == 0){
+    System.out.println(number+ " is an even number!");
+}
+else{
+    System.out.println(number + " is an odd number!");
+}
+
+
+    }
+}
+
