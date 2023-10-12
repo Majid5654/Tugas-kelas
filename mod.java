fee15e9f9bd17df8a03d6b48174fddb2d7c0ad8d
Majid5654 <3rwanmajid@gmail.com>
2023-10-12 20:22:34 +0700

    arkaceken

--- mod.java
+++ mod.java
@@ -3,65 +3,80 @@ import java.util.Scanner;
 public class mod {
     public static void main(String[] args) {
     Scanner scan = new Scanner (System.in);
+    System.out.println("Apakah anda member Bank Qwerty");
+     String member = scan.nextLine();
+
+     if (member.equals("Yes")) {
+         System.out.println("Masukkan nama");
+        String u = scan.next();
+        System.out.println("Masukkan password");
+        int pass = scan.nextInt();
+    
+        if (u.equals("Majid") && pass == 123) {
+                    System.out.println("====================================");
+                    System.out.println("------ Welcome to Bank Qwerty ------");
+                    System.out.println("====================================");
+                    System.out.println("1. Withdraw");
+                    System.out.println("2. Top up");
+                    System.out.println("3. Transfer");
+                    System.out.println("4. Payment");
+                    System.out.println("5. Cash Deposit");
+                    System.out.println("6. Balance Info");
+                    System.out.println("7. Bill");
+        }
+        else if (u.equals("Xavier") && pass == 124 ){
+                    System.out.println("====================================");
+                    System.out.println("------ Welcome to Bank Qwerty ------");
+                    System.out.println("====================================");
+                    System.out.println("1. Withdraw");
+                    System.out.println("2. Top up");
+                    System.out.println("3. Transfer");
+                    System.out.println("4. Payment");
+                    System.out.println("5. Cash Deposit");
+                    System.out.println("6. Balance Info");
+                    System.out.println("7. Bill");
+
+        }
+        else if (u.equals("Arka") && pass == 125){
+                    System.out.println("====================================");
+                    System.out.println("------ Welcome to Bank Qwerty ------");
+                    System.out.println("====================================");
+                    System.out.println("1. Withdraw");
+                    System.out.println("2. Top up");
+                    System.out.println("3. Transfer");
+                    System.out.println("4. Payment");
+                    System.out.println("5. Cash Deposit");
+                    System.out.println("6. Balance Info");
+                    System.out.println("7. Bill");
+        }
+        else if (u.equals("Baihaqi") && pass == 126){
+
+                    System.out.println("====================================");
+                    System.out.println("------ Welcome to Bank Qwerty ------");
+                    System.out.println("====================================");
+                    System.out.println("1. Withdraw");
+                    System.out.println("2. Top up");
+                    System.out.println("3. Transfer");
+                    System.out.println("4. Payment");
+                    System.out.println("5. Cash Deposit");
+                    System.out.println("6. Balance Info");
+                    System.out.println("7. Bill");
+        }
+        else
+            System.out.println("Wrong");
+        
+     }
+    
+     else{
+        System.out.println("Not member canot enter");
+     }
     
     
-System.out.println("Masukkan nama");
-String u = scan.next();
-System.out.println("Masukkan password");
-int pass = scan.nextInt();
- if (u.equals("Majid") && pass == 123) {
-    System.out.println("====================================");
-            System.out.println("-----Selamat Datang Di Bank -----");
-            System.out.println("====================================");
-            System.out.println("1. Account");
-            System.out.println("2. Transfer");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit\n");
-}
-else if (u == "Baihaqi" || pass == 124 ){
-System.out.println("====================================");
-            System.out.println("-----Selamat Datang Di Bank -----");
-            System.out.println("====================================");
-            System.out.println("1. Account");
-            System.out.println("2. Transfer");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit\n");
+    
+    
+        
 
+        }   
 
-}
-else if (u == "Arka" || pass == 125){
-    System.out.println("====================================");
-            System.out.println("-----Selamat Datang Di Bank -----");
-            System.out.println("====================================");
-            System.out.println("1. Account");
-            System.out.println("2. Transfer");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit\n");
-}
-else if (u == "Xavier" || pass == 126){
-
-    System.out.println("====================================");
-            System.out.println("-----Selamat Datang Di Bank -----");
-            System.out.println("====================================");
-            System.out.println("1. Account");
-            System.out.println("2. Transfer");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit\n");
-}
-
-else{
-    System.out.println("salah");
-}
-
-  
-
-
-
-
-
-
-
-}
-
-    }
+ }
 
