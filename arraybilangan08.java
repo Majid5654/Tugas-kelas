46f897d3a5507a80309535d105128a123a94bc97
Majid5654 <3rwanmajid@gmail.com>
2023-10-31 10:08:58 +0700

    loop for

--- arraybilangan08.java
+++ arraybilangan08.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 public class arraybilangan08 {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
-int [] bil = new int [4];
-bil[0]= 5;
-bil[1]= 13;
-bil[2]=-7;
-bil[3]= 17;
+double [] bil = {5.0,12687,7.5,2000000};
 System.out.println(bil[0]);
 System.out.println(bil[1]);
 System.out.println(bil[2]);
