e5ff9fd690e80ce9635e1130d5a94a21d9862acc
Majid5654 <3rwanmajid@gmail.com>
2023-11-01 20:59:07 +0700

    habis mod

--- arraynilai08.java
+++ arraynilai08.java
@@ -4,13 +4,13 @@ public class arraynilai08 {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
 int [] nilaiAkhir = new int[10];
-for (int i = 0;i<10;i++){
+for (int i = 0;i< nilaiAkhir.length;i++){
     System.out.println("Masukkan nilai akhir ke- " +i+" : ");
     nilaiAkhir[i] = scan.nextInt();
 
 }
-for(int i = 0;i<10;i++){
-System.out.println("Nilai akhir ke- " +i+"adalah " +nilaiAkhir[i]);
+
 }
+
 }
-}
+
