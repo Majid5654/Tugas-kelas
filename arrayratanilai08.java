ef6a0ea1f49cad46a4e46d4bd3d74d5c405961b3
Majid5654 <3rwanmajid@gmail.com>
2023-11-03 12:54:12 +0700

    modrata

--- arrayratanilai08.java
+++ arrayratanilai08.java
@@ -2,22 +2,41 @@ import java.util.Scanner;
 public class arrayratanilai08 {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
-int[]nilaiMhs=new int[10];
-    double total = 0;
-    double rata2;
-    int banyak = 0;
-for(int i = 0;i<nilaiMhs.length;i++){
+    int[]nilaiMhs;
+    double totalmin = 0;
+    double totalmaks = 0;
+    double ratamin;
+    double ratamaks;
+    int banyakmin = 0;
+    int jumlah = 0 ;
+    int banyakmaks=0;
+    double tidaklulus = 0;
+System.out.println("Masukkan jumlah mahasiswa : ");
+   jumlah = scan.nextInt();
+   nilaiMhs = new int[jumlah];
+
+for(int i = 0;i<jumlah;i++){
     System.out.println("Masukkan nilai Mahasiswa ke - "+(i+1)+" : ");
     nilaiMhs[i]= scan.nextInt();
-    if(nilaiMhs[i] >70){
-        banyak++;
+
+    //if(nilaiMhs[i]<70){
+       // tidaklulus++;
+   // }
+
     }
+for(int i = 0;i<nilaiMhs.length;i++){
+    if(nilaiMhs[i]<=70){
+        totalmin += nilaiMhs[i];
+        banyakmin++;
     }
-for(int i = 0;i< nilaiMhs.length;i++){
-    total+= nilaiMhs[i];
+    else if(nilaiMhs[i]> 70){
+        totalmaks += nilaiMhs[i];
+        banyakmaks++;
+    }
 }
-rata2= total/nilaiMhs.length;
-System.out.println("Rata rata nilai = " +rata2);
-System.out.println("Banyak siswa yang lulus adalah " +banyak);
+ratamin=  totalmin/banyakmin;
+ratamaks = totalmaks/banyakmaks;
+System.out.println("Rata rata nilai tidak lulus = " +ratamin);
+System.out.println("Rata rata nilai lulus = "+ratamaks );
     }
 }
