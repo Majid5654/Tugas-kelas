c624425e73db3ae87c8083cc63a2b8eeb0facd41
Majid5654 <3rwanmajid@gmail.com>
2023-11-10 13:59:44 +0700

    12

--- cinema08.java
+++ cinema08.java
@@ -16,9 +16,8 @@ System.out.printf("%s \t %s \n",audience[1][0],audience[1][1]);
 System.out.printf("%s \t %s \n",audience[2][0],audience[2][1]);
 System.out.printf("%s \t %s \n",audience[3][0],audience[3][1]);
 System.out.println(audience.length);
-System.out.println("Audience in the row 3 : ");
-for(String i : audience[2]){
-System.out.println(i);
+for(int i = 0;i< audience.length;i++){
+System.out.println("Audience in the row " +(i+1)+ String.join(",", audience [i]));
 }
 
 
