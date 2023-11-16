129ef7a11b700d454bf39fd041e84ff1d55a0816
Majid5654 <3rwanmajid@gmail.com>
2023-11-16 13:29:39 +0700

    baru1

--- individu1.java
+++ individu1.java
@@ -5,15 +5,23 @@ public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
 System.out.println("Masukkan nilai N = ");
 int N = scan.nextInt();
-int i = 1;
-while (i<=N) {
+if (N>=3) {
+   int i = 1;
+    while (i<=N) {
     int j = 1;
-    while (j<i) {
+    while (j<=i) {
         System.out.print(j);
         j++;
     }
     System.out.println();
     i++;
+} 
+    
 }
+else{
+    System.out.println("Minimal angka yang dimasukkan 3 ");
+    
+}
+
     }
 }
