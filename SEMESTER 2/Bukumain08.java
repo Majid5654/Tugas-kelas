154329c9a1a5090e4c5658a89e2e465e6413db73
Majid5654 <3rwanmajid@gmail.com>
2024-02-23 13:51:34 +0700

    bukumain

---
+++ SEMESTER 2/Bukumain08.java
@@ -0,0 +1,19 @@
+public class Bukumain08 {
+public static void main(String[] args) {
+bukuu08 bk1 = new bukuu08();
+bk1.judul = "Today Ends Tomorrow Comes";
+bk1.pengarang = "Denanda Pratiwi";
+bk1.halaman = 198;
+bk1.stok = 13;
+bk1.harga=71000;
+
+bk1.tampilinformasi();
+bk1.terjual(5);
+bk1.gantiHarga(60000);
+bk1.tampilinformasi();
+
+
+
+
+}
+}
