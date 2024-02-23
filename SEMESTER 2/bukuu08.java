c2576ab20905046897d51be1e7ee3bca856cfb35
Majid5654 <3rwanmajid@gmail.com>
2024-02-23 11:23:24 +0700

    smt2

---
+++ SEMESTER 2/bukuu08.java
@@ -0,0 +1,25 @@
+public class bukuu08 {
+String judul,pengarang;
+int halaman,stok,harga;
+void tampilinformasi(){
+    System.out.println("Judul: " +judul);
+    System.out.println("Pengarang : " +pengarang);
+    System.out.println("Jumlah halaman : " +halaman);
+    System.out.println("Sisa stok : " +stok);
+    System.out.println("Harga : Rp " +harga);
+}
+void terjual (int jml){
+    if (stok>0) {
+        stok -= jml; 
+    }
+    else{
+        
+    }
+}
+void restock(int jml){
+    stok+=jml;
+}
+void gantiHarga (int hrg){
+    harga=hrg;
+}
+}
