297982e7a72cba29d910b09c661b50bed967be94
Majid5654 <3rwanmajid@gmail.com>
2024-02-23 15:41:18 +0700

    ud

--- SEMESTER 2/Bukumain08.java
+++ SEMESTER 2/Bukumain08.java
@@ -16,6 +16,10 @@ bukuu08 bk2 = new bukuu08("Self Reward", "Maheera Ayesha", 160, 29, 59000);
 bk2.terjual(11);
 bk2.tampilinformasi();
 
+bukuu08 bukumajid = new bukuu08("Erwan Majid", "Majid Erwan", 160, 29, 59000);
+bukumajid.terjual(11);
+bukumajid.tampilinformasi();
+
 
 }
 }
