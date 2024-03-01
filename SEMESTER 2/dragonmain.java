c4e033ecc52ca4160746aed3dd14eab44f2c5454
Majid5654 <3rwanmajid@gmail.com>
2024-03-01 15:08:32 +0700

    dragon main

---
+++ SEMESTER 2/dragonmain.java
@@ -0,0 +1,19 @@
+public class dragonmain {
+    public static void main(String[] args) {
+        dragon d = new dragon(10,10,25,35);
+        d.printPosition();
+        d.moveRight();d.moveDown();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+        d.moveLeft();
+    }
+    }
+
