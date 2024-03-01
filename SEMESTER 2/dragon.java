9bcda5acf12abda063a0bb365a91cb43b3ba11df
Majid5654 <3rwanmajid@gmail.com>
2024-03-01 15:09:21 +0700

    dragon

---
+++ SEMESTER 2/dragon.java
@@ -0,0 +1,40 @@
+public class dragon {
+    int x, y, width, height;
+    public dragon(int x1, int y1, int w, int h){
+        x = x1;
+        y = y1;
+        width = w;
+        height = h;
+    }
+    void moveLeft(){
+        if(x>0)
+            x = x-1;
+        printPosition();
+        detectCollision();
+    }
+    void moveRight(){
+        if(x<width)
+            x = x+1;
+        printPosition();
+        detectCollision();
+    }
+    void moveUp(){
+        if(y>0)
+            y = y-1;
+        printPosition();
+        detectCollision();
+    }
+    void moveDown(){
+        if(y<height)
+            y = y+1;
+        printPosition();
+        detectCollision();
+    }
+    void printPosition(){
+        System.out.printf("(%d, %d)\n",x,y);
+    }
+    void detectCollision(){
+        if(x<=0 || y<=0 || x>=width || y>=height)
+            System.out.println("Game Over");
+    }
+}
